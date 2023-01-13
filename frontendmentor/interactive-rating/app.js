328528296a8f615b841009810564bb272b3bab2e
guidoaguiar <guidoaguiar@live.com>
2023-01-13 13:01:51 +0000

    test updates

--- frontendmentor/interactive-rating/app.js
+++ frontendmentor/interactive-rating/app.js
@@ -3,20 +3,33 @@ const thanksContainer = document.querySelector(".thanks");
 const submitButton = document.querySelector(".submit-button");
 const rating = document.querySelectorAll(".rating-button");
 const result = document.getElementById("rating-result");
+var ratingFocusList = []
+ratingFocusList.push(rating)
 
-
+// check for focus
 submitButton.addEventListener("click", () => {
   if (result.innerHTML != 0) {
-  thanksContainer.classList.remove("hidden");
-  ratingContainer.style.display = "none";
+    var activeElement = document.activeElement;
+    var myElement = []
+    myElement.push(activeElement)
+    console.log(myElement)
+    console.log(activeElement)
+    if (myElement.includes("rating-button")){
+      thanksContainer.classList.remove("hidden");
+      ratingContainer.style.display = "none";
+    }
   }
   else {
     alert ("Please select one option!")
   }
 });
-
+var pets = []
+pets.push(rating)
 rating.forEach((rate) => {
   rate.addEventListener("click", () => {
     result.innerHTML = rate.innerHTML;
-  });
+    console.log(document.activeElement)
+    console.log(document.querySelectorAll(".rating-button"))
+    console.log(pets.includes('at'));
+    });
 });
